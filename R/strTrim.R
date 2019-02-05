6bd24040ab7637260f88aedfb7a53cacfb63558c
lynuhs <linus@lynuhs.com>
2019-02-05 16:57:30 +0100

    Updates

---
+++ R/strTrim.R
@@ -0,0 +1,12 @@
+#' A string function to remove whitespace
+#'
+#' This function will remove all whitespace before and after a string
+#' @param str The string that should have it's whitespace removed
+#' @export
+#' @examples
+#' strTrim()
+
+strTrim <- function(str){
+  gsub("^\\s+|\\s+$", "", str)
+  return(str)
+}
