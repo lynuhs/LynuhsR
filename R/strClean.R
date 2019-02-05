fd4d457d4d15e0b24170c25ac5804ac4c7153f05
lynuhs <linus@lynuhs.com>
2019-02-05 17:20:55 +0100

    update

---
+++ R/strClean.R
@@ -0,0 +1,12 @@
+#' A string function to remove whitespace
+#'
+#' This function will remove all whitespace before and after a string
+#' @param str The string that should have it's whitespace removed
+#' @export
+#' @examples
+#' strClean(str)
+
+strClean <- function(str){
+  gsub("^\\s+|\\s+$", "", str)
+  return(str)
+}
