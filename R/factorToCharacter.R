95a4be2b9a3f7bddd233fd2ca85bb1f7004aa642
lynuhs <linus@lynuhs.com>
2019-02-05 15:37:09 +0100

    First version

---
+++ R/factorToCharacter.R
@@ -0,0 +1,23 @@
+#' A convertion function
+#'
+#' This function will change a data frame's columns from factor to character
+#' @param data The data frame or factor vector that should be converted
+#' @export
+#' @examples
+#' factorToCharacter()
+
+factorToCharacter <- function(data){
+  if(is.data.frame((data))){
+    for (i in 1:ncol(data)){
+      if(is.factor(data[,i])){
+        data[,i] <- as.character(data[,i])
+      }
+    }
+  } else if (is.factor(data)){
+    data <- as.character(data)
+  } else {
+    stop("Data must be a data frame or factor vector!")
+  }
+
+  return(data)
+}
